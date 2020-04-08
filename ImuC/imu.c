1597bbaa2f81897bbbcd33ac20d145e725a39e20
NickBratsch <nickolasbratsch@yahoo.com>
2020-04-08 12:52:28 -0500

    Checking if op mode is set to compass

--- ImuC/imu.c
+++ ImuC/imu.c
@@ -1,4 +1,4 @@
-/* 
+/*
  *  * tcpclient.c - A simple TCP client
  *   * usage: tcpclient <host> <port>
  *    */
@@ -11,7 +11,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 #include <time.h>
 #include "getbno055.h"
 
@@ -22,17 +22,17 @@
 
 char senaddr[256] = "0x28";
 
- 
-int connect_to_server(int32_t * sockfd);
- 
-void streamData(int32_t sockfd); 
 
-int main() 
+int connect_to_server(int32_t * sockfd);
+
+void streamData(int32_t sockfd);
+
+int main()
 {
     int32_t sockfd;
-    
+
     char buffer[BUFSIZE];
-    
+
     get_i2cbus(senaddr);
 
 
@@ -44,17 +44,17 @@ int main()
 			//Give the server a little time
 			sleep(1);
 		}
-		
-		/*Sending first time registration message*/ 
-		snprintf(buffer, sizeof(buffer), "<BEG>%d$%s<EOF>", ID, "REG"); 
+
+		/*Sending first time registration message*/
+		snprintf(buffer, sizeof(buffer), "<BEG>%d$%s<EOF>", ID, "REG");
 		int n = write(sockfd, buffer, strlen(buffer));
 
 		if(n < 0)
 		{
 			perror("ERROR writing to socket");
 			return -1;
-		}		
-		
+		}
+
 		streamData(sockfd);
 	}
 
@@ -62,24 +62,24 @@ int main()
 }
 
 void streamData(int32_t sockfd)
-{	
-	int res = -1; 
-	int ERR_FLAG = 0; 
+{
+	int res = -1;
+	int ERR_FLAG = 0;
 
-	uint32_t seqID = 0; 
+	uint32_t seqID = 0;
 	uint32_t STREAM = 0;
 	char buf[BUFSIZE];
-	fd_set rfds; 
+	fd_set rfds;
     struct timeval tv;
-	
+
 	while(1)
 	{
        struct bnoacc bnodA;
 	   struct bnoqua bnodQ;
-	   FD_SET(sockfd,&rfds); 
-	   tv.tv_sec = 0; 
+	   FD_SET(sockfd,&rfds);
+	   tv.tv_sec = 0;
 	   tv.tv_usec = 1;
-	    
+
 	   if(select(sockfd+1, &rfds, NULL, NULL, &tv) == -1)
 	   {
 		  perror("Select Error");
@@ -88,33 +88,33 @@ void streamData(int32_t sockfd)
 	   if (FD_ISSET(sockfd, &rfds))
 	   {
 		   //Data is ready to be read
-		   
+
 		   bzero(buf, BUFSIZE);
 		   recv(sockfd, buf, sizeof(buf), 0);
-		   
+
 		   if(!strncmp(buf, "START", 5))
 		   {
-				printf("Stream started\n");   
-				STREAM = 1; 
+				printf("Stream started\n");
+				STREAM = 1;
 		   }
 		   else if(!strncmp(buf, "STOP", 5))
 		   {
-				printf("Stream stopped\n"); 
+				printf("Stream stopped\n");
 				STREAM = 0;
 		   }
 		   else if(!strcmp(buf, ""))
 		   {
-			   return; 
+			   return;
 		   }
 	   }
 	   else if(STREAM)
 	   {
-			//Send data   
+			//Send data
 			if(seqID < 2147483647)
 				seqID++;
 			else
-				seqID = 0; 
-	   
+				seqID = 0;
+
 			bzero(buf, BUFSIZE); //Zero out buffer
 
 			// int mode = get_mode();
@@ -123,11 +123,25 @@ void streamData(int32_t sockfd)
 			// 	ERR_FLAG = 1;
 			// }
 			// else
-			// {	
+			// {
+
+
+			//checking if mode is set correctly
+			int mode = get_mode();
+              if(mode < 8) {
+                 //printf("Error getting Quaternation, sensor mode %d is not a fusion mode.\n", mode);
+                 opmode_t newmode = compass; //compass = 0x09
+                 res = set_mode(newmode);
+                      if(res != 0) {
+                         printf("Error: could not set sensor mode %s [0x%02X].\n", opr_mode, newmode);
+                         exit(-1);
+                      }
+
+              }
 				res = get_acc(&bnodA);
 				if(res != 0) {
 					printf("Error: Cannot read accelerometer data.\n");
-					ERR_FLAG = 1; 
+					ERR_FLAG = 1;
 				}
 
 				// res = get_eul(&bnodE);
@@ -142,31 +156,31 @@ void streamData(int32_t sockfd)
 					ERR_FLAG = 1;
 				}
 			//}
-			
-			/*READ ME: Packet format 
+
+			/*READ ME: Packet format
 			* When sending data each packet will be truncated with an <EOF> string and started with a <BEG> string
 			* Then data is input between using the $ char delimeter
-			* When possible include a char delimiter so data can be pulled off in the highest priority 
+			* When possible include a char delimiter so data can be pulled off in the highest priority
 			* An example is shown below */
 
 			if(!ERR_FLAG)
 			{
-				snprintf(buf, sizeof(buf), "<BEG>%d$%d$%f$%f$%f$%f$%f$%f$%f<EOF>", ID, 
-																			seqID, 
-																			bnodA.adata_x, 
+				snprintf(buf, sizeof(buf), "<BEG>%d$%d$%f$%f$%f$%f$%f$%f$%f<EOF>", ID,
+																			seqID,
+																			bnodA.adata_x,
 																			bnodA.adata_y,
-																			bnodA.adata_z, 
-																			bnodQ.quater_w, 
-																			bnodQ.quater_x, 
+																			bnodA.adata_z,
+																			bnodQ.quater_w,
+																			bnodQ.quater_x,
 																			bnodQ.quater_y,
-																			bnodQ.quater_z); 
+																			bnodQ.quater_z);
 
 				int n = write(sockfd, buf, strlen(buf));
 
 				if(n < 0)
 				{
 					perror("ERROR writing to socket");
-				}		
+				}
 			}
 			sleep(.02);
 	   }
@@ -177,21 +191,21 @@ int connect_to_server(int32_t * sockfd)
 {
 	struct sockaddr_in serveraddr;
     struct hostent *server;
-	
+
 	printf("Connecting to IP: %s on PORT: %d\n", hostname, portno);
-	
+
     /* socket: create the socket */
     *sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (*sockfd < 0) 
+    if (*sockfd < 0)
     {
 	    perror("ERROR opening socket");
 		return 1;
-    }				    
-    
+    }
+
     /* gethostbyname: get the server's DNS entry */
     server = gethostbyname(hostname);
 
-    if (server == NULL) 
+    if (server == NULL)
     {
        fprintf(stderr,"ERROR, no such host as %s\n", hostname);
        return 1;
@@ -202,9 +216,9 @@ int connect_to_server(int32_t * sockfd)
     serveraddr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
     serveraddr.sin_port = htons(portno);
-	
+
     /* connect: create a connection with the server */
-    if (connect(*sockfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) 
+    if (connect(*sockfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
     {
        perror("ERROR connecting");
 	   return 1;
