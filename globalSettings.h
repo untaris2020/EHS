046975894b9cb0facd4cf59f752ee4124e3e1822
Chest <UNTAris2019@gmail.com>
2020-04-17 02:21:34 +0100

    Added Comments

--- globalSettings.h
+++ globalSettings.h
@@ -1,7 +1,5 @@
 // By changing the portno and hostname you will be able to easily change the information on every script necessary
 // To run the chest or glove subsystems.
 
-
-
 #define portno 6002
 #define hostname "127.0.0.1"
