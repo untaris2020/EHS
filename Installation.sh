4dfc9c91eaab2dc0da5795f93231a70c70d12bcc
Chest <UNTAris2019@gmail.com>
2020-04-21 03:05:18 +0100

    Added Installation libraries

--- Installation.sh
+++ Installation.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Updates the Pi
 sudo apt-get update
+
+# Installs Python3
 sudo apt-get install python3
-sudo pip3 install adafruit-circuitpython-bno055
+
+# Installs OpenCv
+sudo apt-get install python3-opencv
+
+# Installs OpenCv C++ Dependencies
 sudo apt-get install libopencv-dev
+
+# Installs Pip3
+sudo apt install python3-pip
+
+# Installs the IMU Library
+sudo pip3 install adafruit-circuitpython-bno055
+
+# Installs the wiringpi Library
 sudo apt-get install wiringpi
